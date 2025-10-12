8b3a52e9db0b583ebd5fceb4f80f9e88de5a5b02
ethanol25 <ethandye04@gmail.com>
2025-10-11 22:45:17 -0500

    fixing?

--- client/lib/auth.tsx
+++ client/lib/auth.tsx
@@ -67,7 +67,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    //throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
 }
